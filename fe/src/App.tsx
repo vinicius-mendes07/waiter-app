193eac386fba2081e0d6f30ef253bacc43ba90b4
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2025-01-24 18:50:06 -0400

    add header

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,11 @@
+import { Header } from "./components/Header";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 export function App() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+    <GlobalStyles />
+    <Header />
+    </>
+  );
 }
