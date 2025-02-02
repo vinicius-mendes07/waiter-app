0371eaa24da2df355a33cd980417bfb1c8f8db7e
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2025-02-02 18:54:51 -0400

    app initial project

---
+++ app/eslint.config.mjs
@@ -0,0 +1,19 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      semi: "error",
+      "@typescript-eslint/no-require-imports": "off",
+    }
+  }
+];
