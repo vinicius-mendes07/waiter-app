f1a2a638261079604812584b8aa718312100d224
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2025-02-03 18:51:40 -0400

    create main page structure

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,6 @@
 import { useFonts } from 'expo-font';
+import { Main } from './src/Main';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { Text } from './src/components/Text';
 
 export default function App() {
   const [isFontsLoaded] = useFonts({
@@ -15,18 +14,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <Main />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/src/Main/index.tsx
@@ -0,0 +1,23 @@
+import { Categories } from "../components/Categories";
+import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
+import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from "./styles";
+
+export function Main() {
+  return (
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
+  );
+}
