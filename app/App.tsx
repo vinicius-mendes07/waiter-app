8e58a2fc8ae49a99031b6ef48f4e3fb0633a3987
Vinicius Mendes <vinicius.henrique.92@hotmail.com>
2025-02-07 19:23:38 -0400

    create product menu

--- app/App.tsx
+++ app/App.tsx
@@ -2,6 +2,9 @@ import { useFonts } from 'expo-font';
 import { Main } from './src/Main';
 import { StatusBar } from 'expo-status-bar';
 
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 export default function App() {
   const [isFontsLoaded] = useFonts({
     'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,3 +1,45 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+import {
+  ProductImage,
+  Product,
+  ProductDetails,
+  Separator,
+  AddToCartButton,
+} from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={(product) => product._id}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      ItemSeparatorComponent={Separator}
+      renderItem={({ item: product }) => (
+        <Product>
+          <ProductImage
+            source={{
+              uri: `http://192.168.1.31:3001/uploads/${product.imagePath}`,
+            }}
+          />
+          <ProductDetails>
+            <Text weight="600">{product.name}</Text>
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+            <Text size={14} weight="600">
+              {formatCurrency(product.price)}
+            </Text>
+          </ProductDetails>
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
+        </Product>
+      )}
+    />
+  );
 }
